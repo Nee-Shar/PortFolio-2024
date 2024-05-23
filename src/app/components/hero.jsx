8dc437805fd7c7f3ad63f1e343aaa6f9f9954900
Neeraj Sharma <neeraj716sharma@gmail.com>
2024-05-23 20:26:44 +0530

    Portfolio Commit

---
+++ src/app/components/hero.jsx
@@ -0,0 +1,37 @@
+"use client";
+import React from "react";
+import Typewriter from "typewriter-effect";
+
+function Hero() {
+  return (
+    <div className="hero min-h-screen bg-primary-content text-white">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <img
+          src="hand-coding-animate.svg"
+          className="max-w-sm rounded-lg shadow-2xl"
+        />
+        <div>
+          <h1 className="text-5xl font-bold">
+            <code>
+              <Typewriter
+                options={{
+                  strings: ["Hello World", "हैलो वर्ल्ड"],
+                  autoStart: true,
+                  loop: true,
+                }}
+              />
+            </code>
+          </h1>
+          <p className="py-6 text-1xl">
+            <blockquote className="italic">
+              Hi, I'm Neeraj Sharma, a full stack web developer—can't fix your
+              Wi-Fi, but can definitely make your website run like a charm.
+            </blockquote>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
---
+++ src/app/components/skills.jsx
@@ -0,0 +1,167 @@
+"use client";
+import React from "react";
+import Zoom from "react-reveal/Zoom";
+function Skills() {
+  return (
+    <div className="bg-primary-content text-white sm:mx-4  " id="skills">
+      <h1 className="mt-4 text-center text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold mb-6">
+        <code>Skills</code>
+      </h1>
+      <Zoom>
+        <div className="mockup-browser border border-base-300 bg-info-content max-w-4xl mx-auto px-4 sm:px-8 sm:mx-auto md:mx-auto w-full  lg:max-w-4xl">
+          <div className="mockup-browser-toolbar">
+            <div className="input border border-base-300">
+              <code>https://nee-shar.com/skills</code>
+            </div>
+          </div>
+          <div className="flex justify-center px-4 py-16 border-t border-base-300">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 2xl:grid-cols-4 justify-items-center gap-5 mt-4">
+              <div>
+                <div className="card w-full  bg-transparent shadow-xl">
+                  {/* Adjusted the width of the card */}
+
+                  <img
+                    width="120px"
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/ISO_C%2B%2B_Logo.svg/911px-ISO_C%2B%2B_Logo.svg.png"
+                    alt="CPP"
+                  />
+                </div>
+              </div>
+              <div>
+                <div className="card w-full  bg-transparent shadow-xl">
+                  {/* Adjusted the width of the card */}
+
+                  <img
+                    width="150px"
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png"
+                    alt="REACT"
+                  />
+                </div>
+              </div>
+              <div>
+                <div className="card w-full  bg-transparent shadow-xl">
+                  {/* Adjusted the width of the card */}
+
+                  <img
+                    width="120px"
+                    src="https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png"
+                    alt="JS"
+                  />
+                </div>
+              </div>
+              <div>
+                <div className="card w-full  bg-transparent shadow-xl">
+                  {/* Adjusted the width of the card */}
+
+                  <img
+                    width="120px"
+                    src="https://static-00.iconduck.com/assets.00/node-js-icon-454x512-nztofx17.png"
+                    alt="NODEJS"
+                  />
+                </div>
+              </div>
+              <div>
+                {" "}
+                <div className="card w-full  bg-transparent shadow-xl">
+                  {/* Adjusted the width of the card */}
+
+                  <img
+                    width="120px"
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1869px-Python-logo-notext.svg.png"
+                    alt="PY"
+                  />
+                </div>
+              </div>
+              <div>
+                {" "}
+                <div className="card w-full  bg-transparent shadow-xl">
+                  {/* Adjusted the width of the card */}
+
+                  <img
+                    width="120px"
+                    src="https://cdn.worldvectorlogo.com/logos/fastapi.svg"
+                    alt="FASTAPI"
+                  />
+                </div>
+              </div>
+              <div>
+                {" "}
+                <div className="card w-full  bg-transparent shadow-xl">
+                  {/* Adjusted the width of the card */}
+
+                  <img
+                    width="120px"
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Firebase_icon.svg/1200px-Firebase_icon.svg.png"
+                    alt="FIREBASE"
+                  />
+                </div>
+              </div>
+              <div>
+                {" "}
+                <div className="card w-full  bg-transparent shadow-xl">
+                  {/* Adjusted the width of the card */}
+
+                  <img
+                    width="150px"
+                    src="https://upload.wikimedia.org/wikipedia/labs/8/8e/Mysql_logo.png"
+                    alt="mysql"
+                  />
+                </div>
+              </div>
+              <div>
+                {" "}
+                <div className="card w-full  bg-transparent shadow-xl">
+                  {/* Adjusted the width of the card */}
+
+                  <img
+                    width="120px"
+                    src="https://seeklogo.com/images/S/supabase-logo-DCC676FFE2-seeklogo.com.png"
+                    alt="SUPABASE"
+                  />
+                </div>
+              </div>
+              <div>
+                {" "}
+                <div className="card w-full  bg-transparent shadow-xl">
+                  {/* Adjusted the width of the card */}
+
+                  <img
+                    width="120px"
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/2560px-Bootstrap_logo.svg.png"
+                    alt="Bootstrap"
+                  />
+                </div>
+              </div>
+              <div>
+                {" "}
+                <div className="card w-full  bg-transparent shadow-xl">
+                  {/* Adjusted the width of the card */}
+
+                  <img
+                    width="100px"
+                    src="https://cdn.worldvectorlogo.com/logos/github-icon-2.svg"
+                    alt="Github"
+                  />
+                </div>
+              </div>
+              <div>
+                {" "}
+                <div className="card w-full  bg-transparent shadow-xl">
+                  {/* Adjusted the width of the card */}
+
+                  <img
+                    width="120px"
+                    src="https://adware-technologies.s3.amazonaws.com/uploads/technology/thumbnail/29/Rlogical-Blog-Images-thumbnail.png"
+                    alt="nextJS"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Zoom>
+    </div>
+  );
+}
+
+export default Skills;
